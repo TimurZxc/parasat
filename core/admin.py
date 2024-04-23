a5a7ead3bca97ac39c391db81f9d2ce54467ebb6
Timur <timur.grig.kz.2002@gmail.com>
2024-04-23 15:28:31 +0500

    front updates

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import *
 
+
+@admin.action(description="Установить изображение по умолчанию")
+def set_preview(modeladmin, request, queryset):
+    queryset.update(preview="default_images/default_video.png")
+
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('title', 'video_id', 'url', 'created_at', 'video_for_main')  # Fields to display in the list view
     readonly_fields = ('video_id',)  # Make 'video_id' read-only
-
+    actions = [set_preview]
     def save_model(self, request, obj, form, change):
         if 'url' in form.changed_data:
             match = re.search(r'\?v=([^&]+)', obj.url)
@@ -16,13 +21,17 @@ class VideoAdmin(admin.ModelAdmin):
         for obj in queryset:
             obj.delete()
 
-
+@admin.action(description="Установить изображение по умолчанию")
+def set_image(modeladmin, request, queryset):
+    queryset.update(image="default_images/default_news.png")
 
 class NewsAdmin(admin.ModelAdmin):
-
+    actions = [set_image]
     def delete_queryset(self, request, queryset):
         for obj in queryset:
             obj.delete()
+
+
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Comment)
--- core/models.py
+++ core/models.py
@@ -12,31 +12,16 @@ class News(models.Model):
     news_for_main = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
-    def save(self, *args, **kwargs):
-        if self.pk:
-            try:
-                old_news = News.objects.get(pk=self.pk)
-                if old_news.image and (not self.image or old_news.image != self.image) and old_news.image.name != 'default_images/default_news.png':
-                    old_image_path = os.path.join(settings.MEDIA_ROOT, old_news.image.name)
-                    if os.path.isfile(old_image_path):
-                        os.remove(old_image_path)
-            except News.DoesNotExist:
-                pass
-        super().save(*args, **kwargs)
+        return self.title 
 
     def update(self, **kwargs):
         for field, value in kwargs.items():
             setattr(self, field, value)
         self.save()
 
-    def delete(self, *args, **kwargs):
-        if self.image and self.image.name != 'default_images/default_news.png':
-            image_path = os.path.join(settings.MEDIA_ROOT, self.image.name)
-            if os.path.isfile(image_path):
-                os.remove(image_path)
-        super().delete(*args, **kwargs)
-
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
 
 class Comment(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comments')
@@ -45,7 +30,9 @@ class Comment(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
     def __str__(self):
         return f'Comment by {self.author_email}'
 
@@ -66,36 +53,14 @@ class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     video_for_main = models.BooleanField(default=False)
-
+    class Meta:
+        verbose_name = 'Видео'
+        verbose_name_plural = 'Видео'
     def __str__(self):
         return f'{self.title} | {self.video_id} | {self.url}'
-
-    def save(self, *args, **kwargs):
-        if self.pk:
-            try:
-                old_video = Video.objects.get(pk=self.pk)
-                if old_video.preview and (not self.preview or old_video.preview != self.preview) and old_video.preview.name != 'default_images/default_video.png':
-                    old_image_path = os.path.join(settings.MEDIA_ROOT, old_video.preview.name)
-                    if os.path.isfile(old_image_path):
-                        os.remove(old_image_path)
-            except Video.DoesNotExist:
-                pass
-        if self.url:
-            match = re.search(r'\?v=([^&]+)', self.url)
-            if match:
-                self.video_id = match.group(1)
-        super().save(*args, **kwargs)
 
     def update(self, **kwargs):
         for field, value in kwargs.items():
             setattr(self, field, value)
         self.save()
-        
-    def delete(self, *args, **kwargs):
-        if self.preview and self.preview.name != 'default_images/default_video.png':
-            image_path = os.path.join(settings.MEDIA_ROOT, self.preview.name)
-            if os.path.isfile(image_path):
-                os.remove(image_path)
-        super().delete(*args, **kwargs)
-
     
